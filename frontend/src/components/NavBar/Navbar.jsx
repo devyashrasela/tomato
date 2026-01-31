aeb29fc36df69c65c0bc22437ab18c7484041330
devyashrasela <devyashrasela@gmail.com>
2026-01-31 19:54:33 +0530

    Updated BAckend File

--- frontend/src/components/NavBar/Navbar.jsx
+++ frontend/src/components/NavBar/Navbar.jsx
@@ -36,7 +36,8 @@ const Navbar = ({ setShowLogin }) => {
                     <div className={getTotalCartAmount()===0 ? "" : "dot"}></div>
                 </div>
                 {!token ? 
-                    <button onClick={() => setShowLogin(true)}>sign in</button> : 
+                    // <button onClick={() => setShowLogin(true)}>sign in</button> : 
+                    <button onClick={() => console.log("Sign in clicked")}>sign in</button> : 
                     <div className='navbar-profile'>
                         <img src={assets.profile_icon} alt="" />
                         <ul className='navbar-profile-dropdown'>
